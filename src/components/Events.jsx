ca85960bba163624d48834e0eb212ed5c56323be
lsundal1 <126904972+lsundal1@users.noreply.github.com>
2025-04-26 22:41:22 +0100

    banner sticks to bottom of page now

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -67,7 +67,7 @@ export default function Events () {
         <div className="flex-1 flex flex-col items-center">
 
         {showSuccess && (
-            <div role="alert" className="alert alert-warning w-full m-3">
+            <div role="alert" className="alert alert-warning fixed bottom-0 left-0 w-full z-50 p-4">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                 </svg>
